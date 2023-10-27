37b191551a6b876fce7a8c16816cd72375219f38
Neeyo1 <jakub.nycz99@gmail.com>
2023-10-27 13:55:06 +0200

    Added login, logout and register functionality

--- movie_app/movies/urls.py
+++ movie_app/movies/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #index
     path("", views.index, name="index"),
+    #login
+    path("login/", views.login_to_page, name="login_to_page"),
+    #logout
+    path("logout/", views.logout_from_page, name="logout_from_page"),
+    #register
+    path("register/", views.register_to_page, name="register_to_page"),
+    #movie
+    #path("movie/<int:movie_id>/", views.movie_detail, name="movie_detail"),
+    #path("movie/create/", views.movie_create, name="movie_create"),
+    #path("movie/edit/<int:movie_id>", views.movie_edit, name="movie_edit"),
+    #path("movie/delete/<int:movie_id>", views.movie_delete, name="movie_delete"),
+    #path("movie/public-private/<int:movie_id>", views.movie_public_private, name="movie_public_private"),
 ]
--- movie_app/movies/views.py
+++ movie_app/movies/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Genre, Tag, Movie
+from .forms import MovieForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -7,3 +12,41 @@ def index(request):
     movies = Movie.objects.all()
     context['movies'] = movies
     return render(request, "movies/index.html", context)
+
+def login_to_page(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("Success")
+            next_url = request.GET.get('next') or reverse('index')
+            return HttpResponseRedirect(next_url)
+        else:
+            error_message = "Incorrect login or password"
+            context["error_message"] = error_message
+            return render(request, "movies/login_form.html", context)
+    return render(request, "movies/login_form.html", context)
+
+def logout_from_page(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register_to_page(request):
+    context = {}
+    form = UserCreationForm()
+    context["form"] = form
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            error_message = "Error during registration"
+            context["error_message"] = error_message
+            context["form"] = form
+            return render(request, "movies/register_form.html", context)
+    return render(request, "movies/register_form.html", context)

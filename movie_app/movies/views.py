a2f93e992e3d22019117bc198620ae9a5cdecf4e
Neeyo1 <jakub.nycz99@gmail.com>
2023-10-27 13:05:31 +0200

    Display movies on index page

--- movie_app/movies/views.py
+++ movie_app/movies/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Genre, Tag, Movie
 
 # Create your views here.
 def index(request):
     context = {}
+    movies = Movie.objects.all()
+    context['movies'] = movies
     return render(request, "movies/index.html", context)

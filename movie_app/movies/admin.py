a4245ef1538c99c2079785044c4238764a99c530
Neeyo1 <jakub.nycz99@gmail.com>
2023-11-06 13:11:12 +0100

    Added profile pictures functionality

--- movie_app/movies/admin.py
+++ movie_app/movies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Genre, Tag, Movie
+from .models import Genre, Tag, Movie, Profile
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Tag)
 admin.site.register(Movie)
+admin.site.register(Profile)
--- movie_app/movies/models.py
+++ movie_app/movies/models.py
@@ -47,3 +47,10 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username

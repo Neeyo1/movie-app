3823fc77b25dced319337f80f28aeec3a50c9739
Neeyo1 <jakub.nycz99@gmail.com>
2023-11-22 12:26:57 +0100

    Added missing rating stars

--- movie_app/movies/templatetags/movies_extras.py
+++ movie_app/movies/templatetags/movies_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name='zip')
 def zip_lists(a, b):
     return zip(a, b)
+
+@register.filter(name='range')
+def filter_range(start, end):
+    return range(start, end)
